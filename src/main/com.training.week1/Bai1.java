db84081fec2436925fb1e89f3f98ecd49df9889e
MRcat0208 <hailam1069@gmail.com>
2021-09-17 20:38:38 +0700

    Initial commit

---
+++ src/main/com.training.week1/Bai1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bai1
+{
+    /*public static void main(String[] args) {
+
+        int a = 5, b = 15, sum;
+        sum = a + b;
+
+        System.out.println("Tổng của hai số là: "+sum);
+    }*/
+
+    public static void main(String[] args) {
+
+        int a, b, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số thứ nhất: ");
+        a = sc.nextInt();
+
+        System.out.println("Nhập vào số thứ hai: ");
+        b = sc.nextInt();
+
+        sc.close();
+        sum = a + b;
+        System.out.println("Tổng của hai số vừa nhập là: "+sum);
+    }
+}
---
+++ src/main/com.training.week1/Bai4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        int a, b, X;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số thứ nhất: ");
+        a = sc.nextInt();
+
+        System.out.println("Nhập vào số thứ hai: ");
+        b = sc.nextInt();
+
+        sc.close();
+        X = a * b;
+        System.out.println("Tích của hai số vừa nhập là: " + X);
+    }
+}
